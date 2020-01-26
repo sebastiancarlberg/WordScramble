eefce61ca856ac6594ffee0eeae1c12c23739739
Sebastian Carlberg <carlberg.sebastian@gmail.com>
2020-01-26 21:10:53 +0100

    Initial Commit

---
+++ WordScramble/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  WordScramble
+//
+//  Created by Sebastian Carlberg on 2020-01-26.
+//  Copyright © 2020 Sebastian Carlberg. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
